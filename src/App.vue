586119c8e5e6bd4127b53fd7d1247a0ab4df1d69
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-03-19 01:41:21 +1100

    1:00:00

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Shopping Manager App"/>
+<!--    <my-button v-if="show" :key="1"/>-->
+<!--    <my-button v-if="!show" :key="2"/>-->
+    <my-button>
+      <template #append>
+      +
+      </template>
+      Кол-во кликов
+    </my-button>
+
+    <my-button>
+      Нажатые кнопки
+    </my-button>
+<!--    <button @click="show=!show">Show / Hide</button>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyButton from "./components/MyButton.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton
+  },
+  data(){
+    return {
+      show: false
+    }
   }
 }
 </script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <slot name="append" />
+        <button @click="onClick"><slot>Clicked</slot>{{ counter }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyButton",
+        props: {
+            msg: String
+        },
+        data(){
+            return {
+                counter: 0,
+            }
+        },
+        methods: {
+            onClick(){
+                this.counter++;
+            },
+            onMouseOver(){
+                console.log('mouseover');
+            }
+        },
+        beforeCreate() {
+            console.log('beforeCreate')
+        },
+        created() {
+            console.log('Created')
+        },
+        beforeMount() {
+            console.log('before Mount')
+        },
+        mounted() {
+            const btn = this.$el.querySelector('button');
+            if (btn){
+                btn.addEventListener('mouseover', this.onMouseOver);
+            }
+            console.log('mounted');
+        },
+        beforeUpdate() {
+            console.log('Before Update')
+        },
+        updated() {
+            console.log('Updated')
+        },
+        beforeDestroy() {
+            console.log('before Destroy');
+            const btn = this.$el.querySelector('button');
+            btn.removeEventListener('mouseover', this.onMouseOver);
+        },
+        destroyed() {
+            console.log('Destroyed');
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
