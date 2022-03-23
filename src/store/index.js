d603cb8143c358290d637b89607ab5a8b41c3f07
isakhalin <isakhalin@gmail.com>
2022-03-23 22:46:23 +1100

    27:00

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+      paymentList: []
   },
   getters: {
   },
   mutations: {
+    setPaymentListData(state, payload){
+
+    }
   },
   actions: {
   },
